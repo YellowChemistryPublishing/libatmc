dba1c1965e51a852b6bb43bb4a7f6b8c90b8cd91
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-05-22 13:38:17 +1000

    Remove libcxxext files.

--- libcxxext/core/module/sys.Containers.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <InplaceAtomicSet.h>
-#include <InplaceQueue.h>
-#include <InplaceSet.h>
-#include <InplaceString.h>
-#include <InplaceVector.h>
--- libcxxext/core/module/sys.Threading.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <SpinLock.h>
-#include <Task.h>
-#include <Thread.h>
